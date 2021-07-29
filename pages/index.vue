a33991827c46dfe161a5627e2c8f0faf62668315
Haruki-MacBookPro <coral.reef610@gmail.com>
2021-07-29 22:54:05 +0900

    fix: 文字削除

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <b-carousel :indicator="false">
         <b-carousel-item class="main-image-box" v-for="(item, i) in 6" :key="i">
           <b-image class="image main-image" ratio="6by4" :src="require(`@/assets/images/top/top-${i+1}.JPG`)"></b-image>
-          <img class="main-title" src="@/assets/images/top/main_title.svg" />
+          <!-- <img class="main-title" src="@/assets/images/top/main_title.svg" /> -->
         </b-carousel-item>
       </b-carousel>
     </div>
